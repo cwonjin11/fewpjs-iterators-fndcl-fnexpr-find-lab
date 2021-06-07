24b7d6d373c3af93a45d4e8b78784c6b89bfc118
Wonjin Cho <cwonjin11@gmail.com>
2021-06-07 12:07:20 -0400

    commit tree 0a1e7e10d8bdb7e23a0e031fd455512f0920cc64

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2020", result: "W"},
+    {year: "2021", result: "W"},
+    {year: "2022", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ] 
+
+superbowlWin = (record) => {
+    let result = record.find( record => record.result === "W" )
+    return !!result ? result.year : undefined
+  }
